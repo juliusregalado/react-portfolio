d5b125675227d02fbd9850b19338541e56a8d4e5
Julius Regalado <jreg03@outlook.com>
2022-04-08 19:15:24 -0400

    add hero section with independents

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,23 +1,19 @@
 import React from 'react'
 
-export default function HeroSection() {
+const HeroSection = () => {
     let user = {
         name: 'Julius Regalado',
-        email: 'jreg03@outlook.com',
-        title: 'Web Dev',
-        sector: 'eCommerce',
-        linkedinHandle: 'https://www.linkedin.com/in/juliusregalado/'
+        title: 'Web Dev'
     }
   return (
     <div className='hero-section'>
-      <div className='hero-inner-container'>
-          <div className='hero-copy-block'>
-            <h1>{user.name}</h1>
-            <p>{user.title}</p>
-            <a href={user.linkedninHandle}>linkedin.com/in/juliusregalado</a>
-          </div>
-      </div>
+        <div className='hero-inner-container'>
+            <div className='hero-copy-block'>
+                <h1>{user.name}</h1>
+                <p>{user.title}</p>
+            </div>
+        </div>
     </div>
-    
   )
 }
+export default HeroSection;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
+import './components/App.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
