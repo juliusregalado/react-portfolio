92baab41d992162ac2f0901286893775eae84a5d
Julius Regalado <jreg03@outlook.com>
2022-04-10 09:00:02 -0400

    logo updates

---
+++ src/components/Sections/ProjectsSection.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const ProjectsSection = () => {
+    const services = ['E-Commerce', 'Responsive Design', 'Web Security']
+  return (
+    <div>ProjectsSection</div>
+  )
+}
